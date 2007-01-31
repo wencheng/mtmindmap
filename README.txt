cce9c039d59fa76e0e1c7878a9f37296e273e187
wencheng.fang <wencheng.fang@5e69bc10-5127-0410-82ca-eb853cbb2ca5>
2007-01-31 11:59:44 +0000

    License notice added

--- README.txt
+++ README.txt
@@ -1,2 +1,13 @@
+1. Introduction
+2. License
+
+============
+Introduction
+============
 Please goto http://code.google.com/p/mtmindmap/ to get everything about MTMindmap.
 Anyway they are may not exactly what you are looking for. :-)
+
+=======
+License
+=======
+MTMindmap is licensed under the Apache License, Version 2.0.
--- lib/Mindmap/L10N/ja.pm
+++ lib/Mindmap/L10N/ja.pm
@@ -1,11 +1,24 @@
+# Copyright 2007 Wencheng Fang
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 package Mindmap::L10N::ja;
 
 use strict;
 use base 'Mindmap::L10N::en_us';
 use vars qw( %Lexicon );
 
-## The following is the translation table.
-
+# The following is the translation table.
 %Lexicon = (
     'The Plugin to display category as a mindmap' => 'カテゴリをマインドマップで表示するプラグインです。',
     'Show blog name:' => 'ブログ名表示:',
